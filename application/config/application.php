b8cc8db184ffd491b116c94ec3633622f1fc978e
Corey Wynsma <corey@wynsma.com>
2014-10-26 18:56:52 -0400

    Added config files for application and session

---
+++ application/config/application.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Config
+|--------------------------------------------------------------------------
+|
+*/
+
+return array(
+
+
+  /*
+  |-----------------------------------
+  | URL
+  |-----------------------------------
+  |
+  | The URL of your application.
+  |
+  */
+
+
+  'url' => array(
+
+    'default' => 'http://www.wijbe.com',
+
+    'local' => 'http://localhost:8080',
+
+  ),
+
+
+
+  /*
+  |-----------------------------------
+  | Index
+  |-----------------------------------
+  |
+  | The controller/method to be served
+  | as your application's index
+  |
+  */
+
+  'index' => array(
+
+    'default' => '/welcome/index',
+
+  ),
+
+
+
+
+);
